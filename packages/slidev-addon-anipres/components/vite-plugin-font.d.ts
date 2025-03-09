c5d62761a8d31237fa15caf0180ce8c1894b16ff
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-09 10:46:15 +0900

    Add comments

--- packages/slidev-addon-anipres/components/vite-plugin-font.d.ts
+++ packages/slidev-addon-anipres/components/vite-plugin-font.d.ts
@@ -1,3 +1,7 @@
+// `vite-plugin-font` resolves `*.ttf` file imports.
+// It provides the type definition for the `*.ttf` imports,
+// but it misses some properties,
+// so we need to declare them here.
 declare module "@konghayao/_font_" {
   export const fontFamilyFallback: string;
 }
