7fe5cd4c9540a8688587ff05845c6ebea32102b1
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-11-03 01:40:00 +0900

    Generalize the cache

--- packages/anipres/src/cache.ts
+++ packages/anipres/src/cache.ts
@@ -1,44 +1,41 @@
 import { AnimationController } from "./animation";
 import { EditorSignals } from "./editor-signals";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const controllerCache = new WeakMap<any, any>();
 const INSTANCE_KEY = Symbol("instance");
 
-export function getAnimationController(
-  ...args: ConstructorParameters<typeof AnimationController>
-): AnimationController {
-  let map = controllerCache;
-  for (const arg of args) {
-    if (!map.has(arg)) map.set(arg, new WeakMap());
-    map = map.get(arg);
-  }
-
-  if (map.has(INSTANCE_KEY)) {
-    return map.get(INSTANCE_KEY) as AnimationController;
-  }
-
-  const instance = new AnimationController(...args);
-  map.set(INSTANCE_KEY, instance);
-  return instance;
-}
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const editorSignalsCache = new WeakMap<any, any>();
-export function getEditorSignals(
-  ...args: ConstructorParameters<typeof EditorSignals>
-): EditorSignals {
-  let map = editorSignalsCache;
-  for (const arg of args) {
-    if (!map.has(arg)) map.set(arg, new WeakMap());
-    map = map.get(arg);
-  }
+export function singletonize<T extends new (...args: any) => InstanceType<T>>(
+  klass: T,
+): (...args: ConstructorParameters<T>) => InstanceType<T> {
+   
+  type CacheMap = WeakMap<
+    ConstructorParameters<T>[number] | symbol,
+    CacheMap | InstanceType<T>
+  >;
+  const cache: CacheMap = new WeakMap();
 
-  if (map.has(INSTANCE_KEY)) {
-    return map.get(INSTANCE_KEY) as EditorSignals;
-  }
+  return (...args) => {
+    let map = cache;
+    for (const arg of args) {
+      let nextMap = map.get(arg);
+      if (!nextMap) {
+        nextMap = new WeakMap();
+        map.set(arg, nextMap);
+      }
+      map = nextMap as CacheMap;
+    }
 
-  const instance = new EditorSignals(...args);
-  map.set(INSTANCE_KEY, instance);
-  return instance;
+    const maybeInstance = map.get(INSTANCE_KEY);
+    if (maybeInstance) {
+      return maybeInstance as InstanceType<T>;
+    }
+
+    const instance = new klass(...args);
+    map.set(INSTANCE_KEY, instance);
+    return instance;
+  };
 }
+
+export const getAnimationController = singletonize(AnimationController);
+
+export const getEditorSignals = singletonize(EditorSignals);
