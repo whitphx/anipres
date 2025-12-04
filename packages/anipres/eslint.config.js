bdf6ce3d35114f30ad8a22c218f66d301e56e58b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-12-04 18:37:04 +0900

    Set up anipres/eslint.config.js

--- packages/anipres/eslint.config.js
+++ packages/anipres/eslint.config.js
@@ -1,24 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
+// @ts-check
+
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslint from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default defineConfig(
+  globalIgnores(["dist"]),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
+  },
+  {
     rules: {
-      ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
