1e1c3a47039f0acada0d10a00fe7ef5e836170e1
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-12-05 01:21:09 +0900

    Memoize DraggableFrameUI

--- packages/anipres/src/Timeline/DraggableFrameUI.tsx
+++ packages/anipres/src/Timeline/DraggableFrameUI.tsx
@@ -3,55 +3,58 @@ import { useDraggable } from "@dnd-kit/core";
 import { useDraggableFrameDelta } from "./FrameMoveTogetherDndContext";
 import { Frame } from "../models";
 
-export function DraggableFrameUI({
-  id,
-  trackId,
-  trackIndex,
-  globalIndex,
-  frame,
-  children,
-  className,
-}: {
-  id: string;
-  trackId: string;
-  trackIndex: number;
-  globalIndex: number;
-  frame: Frame;
-  children: React.ReactNode;
-  className?: string;
-}) {
-  const { attributes, listeners, setNodeRef, isDragging, active } =
-    useDraggable({
-      id,
-      data: {
-        trackId,
-        trackIndex,
-        globalIndex,
-        frame,
-      },
-    });
-  const { registerDOM, deltaX } = useDraggableFrameDelta(trackId, trackIndex);
-  const transformX = deltaX ?? 0;
-  const transformY = 0;
-  const isDraggingSomething = active != null;
-  const style: React.CSSProperties = {
-    transform: `translate(${transformX}px, ${transformY}px)`,
-    transition: isDraggingSomething ? undefined : "transform 0.3s",
-    cursor: isDragging ? "grabbing" : "grab",
-  };
+export const DraggableFrameUI = React.memo(
+  ({
+    id,
+    trackId,
+    trackIndex,
+    globalIndex,
+    frame,
+    children,
+    className,
+  }: {
+    id: string;
+    trackId: string;
+    trackIndex: number;
+    globalIndex: number;
+    frame: Frame;
+    children: React.ReactNode;
+    className?: string;
+  }) => {
+    const { attributes, listeners, setNodeRef, isDragging, active } =
+      useDraggable({
+        id,
+        data: {
+          trackId,
+          trackIndex,
+          globalIndex,
+          frame,
+        },
+      });
+    const { registerDOM, deltaX } = useDraggableFrameDelta(trackId, trackIndex);
+    const transformX = deltaX ?? 0;
+    const transformY = 0;
+    const isDraggingSomething = active != null;
+    const style: React.CSSProperties = {
+      transform: `translate(${transformX}px, ${transformY}px)`,
+      transition: isDraggingSomething ? undefined : "transform 0.3s",
+      cursor: isDragging ? "grabbing" : "grab",
+    };
 
-  return (
-    <div
-      ref={(node) => {
-        setNodeRef(node);
-        registerDOM(node);
-      }}
-      {...attributes}
-      {...listeners}
-      style={style}
-      className={className}
-    >
-      {children}
-    </div>
-  );
-}
+    return (
+      <div
+        ref={(node) => {
+          setNodeRef(node);
+          registerDOM(node);
+        }}
+        {...attributes}
+        {...listeners}
+        style={style}
+        className={className}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+DraggableFrameUI.displayName = "DraggableFrameUI";
